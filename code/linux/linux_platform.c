342b45cd312bc0ab312101de463d4c7eca7e0609
WinstThePhi <winstonhvo@gmail.com>
2021-03-15 12:56:46 -0700

    Wrote templates for the Linux memory functions.

--- code/linux/linux_platform.c
+++ code/linux/linux_platform.c
@@ -2,11 +2,24 @@
 #include "../layer.h"
 #include "linux_platform.h"
 
-internal f32 get_time()
+internal f32 
+get_time()
 {
 	struct timespec time_spec_thing = {0};
 	clock_gettime(CLOCK_REALTIME, &time_spec_thing);
 	float seconds_elapsed = 
 		((float)time_spec_thing.tv_nsec / (float)1e9);
 	return seconds_elapsed;
+}
+
+internal void *
+request_mem(u32 size)
+{
+        
+}
+
+internal 
+void free_mem(void *mem)
+{
+        
 }
--- code/linux/linux_platform.h
+++ code/linux/linux_platform.h
@@ -3,4 +3,9 @@
 
 internal f32 get_time();
 
+// TODO(winston): write the linux memory functions
+internal void *request_mem(u32 size);
+
+internal void free_mem(void *mem);
+
 #endif
